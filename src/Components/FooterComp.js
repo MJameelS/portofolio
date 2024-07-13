3f1755822260901ebe9d713d3b63fd6669a61e96
Muhammad Jameel Shafa <muhammadjameelshafa@gmail.com>
2024-07-13 09:13:56 +0700

    merubah bagian footer yang sebelumnya ada whatsapp, seetelah itu whatsapp di hapus

--- src/Components/FooterComp.js
+++ src/Components/FooterComp.js
@@ -10,10 +10,15 @@ const FooterComp = () => {
             <h3 className="fw-bold text-white text-center">Ngoding</h3>
           </Col>
           <div className="text-center mt-2">
-            <i class="fa-brands fa-facebook text-white fs-1 px-2"></i>
-            <i class="fa-brands fa-whatsapp text-white fs-1 px-2"></i>
-            <i class="fa-brands fa-instagram text-white fs-1 px-2"></i>
-            <i class="fa-brands fa-linkedin text-white fs-1 px-2"></i>
+            <a href="https://www.facebook.com/MuhammadJameelShafa">
+              <i class="fa-brands fa-facebook text-white fs-1 px-2"></i>
+            </a>
+            <a href="https://www.instagram.com/m.jameelshafa">
+              <i class="fa-brands fa-instagram text-white fs-1 px-2"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/muhammad-jameel-shafa-44b253230/" target="_blank" rel="noopener noreferrer">
+              <i class="fab fa-linkedin text-white fs-1 px-2"></i>
+            </a>
           </div>
         </Row>
         <Row>
